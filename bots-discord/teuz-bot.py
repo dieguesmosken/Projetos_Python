743b3ee67b37187df0455477990f97d2ebd9283a
dev.teuz <matheus.diegues@fatec.sp.gov.br>
2023-08-11 16:10:16 -0300

    alterações revertidas

--- bots-discord/teuz-bot.py
+++ bots-discord/teuz-bot.py
@@ -1,13 +1,11 @@
 import discord
 from discord.ext import commands
-from discord_slash import SlashCommand
 
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-slash = SlashCommand(bot, sync_commands=True)
 
 @bot.event
 async def on_ready():
@@ -15,24 +13,22 @@ async def on_ready():
     print("-="*10)
     print("escrito por @dieguesmosken")
 
-@slash.slash(name="add_role", description="Adiciona um cargo a um membro")
+@bot.command()
 async def add_role(ctx, member: discord.Member, role: discord.Role):
     await member.add_roles(role)
-    await ctx.send(f'{member.name} has been given the {role.name} role.')
-    print(f'{member.name} has been given the {role.name} role.')
+    await ctx.send(f'{member.name} recebeu o cargo {role.name}.')
 
-@slash.slash(name="add_color", description="Altera a cor de um cargo")
+@bot.command()
 async def add_color(ctx, role: discord.Role, color: discord.Color):
     await role.edit(color=color)
-    await ctx.send(f'The {role.name} role has been given the {color} color.')
-    print(f'The {role.name} role has been given the {color} color.')
+    await ctx.send(f'O cargo {role.name} recebeu a cor {color}.')
 
-@slash.slash(name="greet", description="Cumprimenta o bot")
+@bot.command()
 async def greet(ctx):
-    await ctx.send("Olá! Como posso ajudar?")
+    await ctx.send('Olá! Em que posso ajudar?')
 
-@slash.slash(name="clear", description="Limpa mensagens no canal")
-async def clear(ctx, amount=5):
+@bot.command()
+async def clear(ctx, amount: int = 5):
     await ctx.channel.purge(limit=amount + 1)
 
 bot.run('MTEzOTYwOTAzMjA5NzgwNDM4OQ.GTpEqf.67NZa7mmqLGMB1wcv8fY9Qa8G2xhW98WJLF1AE')
