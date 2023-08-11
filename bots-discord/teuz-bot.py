22022609886632fbb373ae4867952e1310a47ee6
dev.teuz <matheus.diegues@fatec.sp.gov.br>
2023-08-11 16:02:04 -0300

    alterações no bot

--- bots-discord/teuz-bot.py
+++ bots-discord/teuz-bot.py
@@ -1,20 +1,38 @@
 import discord
 from discord.ext import commands
+from discord_slash import SlashCommand
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+slash = SlashCommand(bot, sync_commands=True)
 
 @bot.event
 async def on_ready():
-    print('Teuz Bot is ready.')
+    print('teuz bot esta pronto!')
+    print("-="*10)
+    print("escrito por @dieguesmosken")
 
-@bot.command()
+@slash.slash(name="add_role", description="Adiciona um cargo a um membro")
 async def add_role(ctx, member: discord.Member, role: discord.Role):
     await member.add_roles(role)
     await ctx.send(f'{member.name} has been given the {role.name} role.')
+    print(f'{member.name} has been given the {role.name} role.')
 
-@bot.command()
+@slash.slash(name="add_color", description="Altera a cor de um cargo")
 async def add_color(ctx, role: discord.Role, color: discord.Color):
     await role.edit(color=color)
     await ctx.send(f'The {role.name} role has been given the {color} color.')
+    print(f'The {role.name} role has been given the {color} color.')
 
-bot.run('ea6b895ff09eeaa6d76b80012de35fe1b9fc67b186380afb341abe2f5b28df66')
+@slash.slash(name="greet", description="Cumprimenta o bot")
+async def greet(ctx):
+    await ctx.send("Olá! Como posso ajudar?")
+
+@slash.slash(name="clear", description="Limpa mensagens no canal")
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount + 1)
+
+bot.run('MTEzOTYwOTAzMjA5NzgwNDM4OQ.GTpEqf.67NZa7mmqLGMB1wcv8fY9Qa8G2xhW98WJLF1AE')
