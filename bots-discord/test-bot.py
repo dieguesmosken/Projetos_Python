9493f007e4fe5fdc7b680669cf5fe4c66f3d0818
dev.teuz <matheus.diegues@fatec.sp.gov.br>
2023-08-12 23:15:56 -0300

    mudanças e correções

--- bots-discord/test-bot.py
+++ bots-discord/test-bot.py
@@ -1,71 +1,73 @@
 import os
 import discord
 import datetime
-from discord.ext import commands
 from config import DISCORD_TOKEN
 
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
-intents.guilds = True  # Adicione esta linha
+intents.guilds = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+client = discord.Client(intents=intents)
 
 TOKEN = DISCORD_TOKEN
 message = "DSM Fatec Registro"
 
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord! está pronto!')
+    print(f'{client.user.name} has connected to Discord! está pronto!')
     print("-="*10)
-    #await bot.change_presence(activity=discord.Game(name=message)) #padrão
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name='Dominando o mundo')) #stremando
-    #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='Música test')) #ouvindo
-    #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Filmes')) #assistindo
-    #await bot.change_presence(activity=discord.Streaming(name=message, url='https://github.com/dieguesmosken')) #transmitindo
+    await client.change_presence(activity=discord.Streaming(name=message, url='https://github.com/dieguesmosken')) #transmitindo
     print("-="*10)
     print("escrito por @dieguesmosken")
     print("-="*10)
     print(f"status alterado para {message} !")
 
-@bot.event
+
+@client.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(f'OI {member.name}, Seja bem vindo ao TEST!')
 
 
-@bot.command()
-async def saudacao(ctx):
-    hora_atual = datetime.datetime.now().time()
-    mensagem = ""
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-    if hora_atual < datetime.time(12):
-        mensagem = f"Bom dia {ctx.author.name}, como vai você?"
-    elif hora_atual < datetime.time(18):
-        mensagem = f"Boa tarde {ctx.author.name}, como vai você?"
-    else:
-        mensagem = f"Boa noite {ctx.author.name}, como vai você?"
+    if message.content.startswith('!saudacao'):
+        hora_atual = datetime.datetime.now().time()
+        mensagem = ""
 
-    await ctx.send(mensagem)
+        if hora_atual < datetime.time(12):
+            mensagem = f"Bom dia {message.author.name}, como vai você?"
+        elif hora_atual < datetime.time(18):
+            mensagem = f"Boa tarde {message.author.name}, como vai você?"
+        else:
+            mensagem = f"Boa noite {message.author.name}, como vai você?"
 
-@bot.command()
-async def add_role(ctx, member: discord.Member, role: discord.Role):
-    await member.add_roles(role)
-    await ctx.send(f'{member.name} recebeu o cargo {role.name}.')
-
-@bot.command()
-async def add_color(ctx, role: discord.Role, color: discord.Color):
-    await role.edit(color=color)
-    await ctx.send(f'O cargo {role.name} recebeu a cor {color}.')
-
-@bot.command()
-async def greet(ctx):
-    await ctx.send('Olá! Em que posso ajudar?')
-
-@bot.command()
-async def clear(ctx, amount: int = 5):
-    await ctx.channel.purge(limit=amount + 1)
+        await message.channel.send(mensagem)
 
 
-bot.run(TOKEN)
+@client.event
+async def on_raw_message_delete(payload):
+    print(f'Mensagem deletada: {payload.message_id}')
+
+
+@client.event
+async def on_raw_reaction_add(payload):
+    print(f'Reação adicionada: {payload.emoji}')
+
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    print(f'Reação removida: {payload.emoji}')
+
+
+@client.event
+async def on_raw_bulk_message_delete(payload):
+    print(f'Várias mensagens deletadas: {payload.message_ids}')
+
+
+client.run(TOKEN)
