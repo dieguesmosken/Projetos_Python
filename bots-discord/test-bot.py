7c65c26b40942fe519bf28bfc706d6f0a4cca91c
dev.teuz <matheus.diegues@fatec.sp.gov.br>
2023-08-12 23:07:12 -0300

    salve 2

--- bots-discord/test-bot.py
+++ bots-discord/test-bot.py
@@ -1,24 +1,71 @@
-# bot.py
 import os
-
 import discord
-from dotenv import load_dotenv
+import datetime
+from discord.ext import commands
 from config import DISCORD_TOKEN
 
-load_dotenv()
-TOKEN = os.getenv(DISCORD_TOKEN)
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+intents.guilds = True  # Adicione esta linha
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
+TOKEN = DISCORD_TOKEN
+message = "DSM Fatec Registro"
+
+
+@bot.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord! está pronto!')
+    print("-="*10)
+    #await bot.change_presence(activity=discord.Game(name=message)) #padrão
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.streaming, name='Dominando o mundo')) #stremando
+    #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='Música test')) #ouvindo
+    #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Filmes')) #assistindo
+    #await bot.change_presence(activity=discord.Streaming(name=message, url='https://github.com/dieguesmosken')) #transmitindo
+    print("-="*10)
+    print("escrito por @dieguesmosken")
+    print("-="*10)
+    print(f"status alterado para {message} !")
 
-@client.event
+@bot.event
 async def on_member_join(member):
     await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
+    await member.dm_channel.send(f'OI {member.name}, Seja bem vindo ao TEST!')
 
-client.run(TOKEN)
+
+@bot.command()
+async def saudacao(ctx):
+    hora_atual = datetime.datetime.now().time()
+    mensagem = ""
+
+    if hora_atual < datetime.time(12):
+        mensagem = f"Bom dia {ctx.author.name}, como vai você?"
+    elif hora_atual < datetime.time(18):
+        mensagem = f"Boa tarde {ctx.author.name}, como vai você?"
+    else:
+        mensagem = f"Boa noite {ctx.author.name}, como vai você?"
+
+    await ctx.send(mensagem)
+
+@bot.command()
+async def add_role(ctx, member: discord.Member, role: discord.Role):
+    await member.add_roles(role)
+    await ctx.send(f'{member.name} recebeu o cargo {role.name}.')
+
+@bot.command()
+async def add_color(ctx, role: discord.Role, color: discord.Color):
+    await role.edit(color=color)
+    await ctx.send(f'O cargo {role.name} recebeu a cor {color}.')
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send('Olá! Em que posso ajudar?')
+
+@bot.command()
+async def clear(ctx, amount: int = 5):
+    await ctx.channel.purge(limit=amount + 1)
+
+
+bot.run(TOKEN)
